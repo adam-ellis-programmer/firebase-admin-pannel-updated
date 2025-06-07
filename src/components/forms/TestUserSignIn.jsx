8b4851048078d9105e57f39b7b1e16cb364bbc68
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-06-07 15:03:06 +0100

    test user button added

--- src/components/forms/TestUserSignIn.jsx
+++ src/components/forms/TestUserSignIn.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+
 const TestUserSignIn = () => {
+  const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
   const handleClick = async () => {
     try {
+      setLoading(true)
       const auth = getAuth()
       const user = await signInWithEmailAndPassword(
         auth,
@@ -12,14 +16,23 @@ const TestUserSignIn = () => {
         '111111'
       )
       navigate('/')
+      setLoading(false)
     } catch (error) {
       console.log(error)
+      setLoading(false)
     }
   }
   return (
     <div className='test-btn-wrap'>
       <button type='button' className='test-btn' onClick={handleClick}>
-        Test Drive App
+        {loading ? (
+          <div className='test-btn-inner-wrap'>
+            <p> checking </p>
+            <div className='loader'></div>
+          </div>
+        ) : (
+          'Test Drive App'
+        )}
       </button>
     </div>
   )
